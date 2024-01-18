46a1c60069867b8e1fb9c272457a13cccea1e681
Kastney Frazão <kastney@outlook.com>
2024-01-18 16:03:51 -0300

    🚀 chore - Publishing library version 1.0.0-alpha

--- Source/Extension Manager/Models/PluginItem.cs
+++ Source/Extension Manager/Models/PluginItem.cs
@@ -1,7 +1,12 @@
 ﻿namespace Nulo.Modules.ExtensionManager {
 
     public sealed class PluginItem(string productName, Assembly assembly) {
+
+        #region Properties
+
         public string ProductName { get; private set; } = productName.Replace(" ®", "®");
         public Assembly Assembly { get; private set; } = assembly;
+
+        #endregion Properties
     }
 }
